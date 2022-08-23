a7c204eda0db7a9487b17c3c7eec06a03d89c90b
Asif <mrasifkhan055@gmail.com>
2022-08-23 08:14:05 -0700

    remove super command constructor

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // this is some super constructor
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
